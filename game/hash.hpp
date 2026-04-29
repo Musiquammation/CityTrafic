c53e5fc4aba3ae0c056af740d4c3896e115ca535
Musiquammation <bryanmegherbi@gmail.com>
2026-04-29 20:23:51 +0200

    Remove unuseful #ifdef

--- game/hash.hpp
+++ game/hash.hpp
@@ -3,10 +3,7 @@
 #include <array>
 #include <stdint.h>
 
-#ifndef HASH_TYPE
-#define HASH_TYPE
 typedef uint64_t hash_t;
-#endif
 
 uint64_t hash_generate();
 void hash_longCopy(uint8_t* dst, const uint8_t* src);
