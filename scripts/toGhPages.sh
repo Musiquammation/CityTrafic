2531133b2a81409f7b8f1df6a3124e307c559797
Musiquammation <bryanmegherbi@gmail.com>
2026-05-13 22:45:46 +0200

    Complete toGhPages.sh

--- scripts/toGhPages.sh
+++ scripts/toGhPages.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+make emcc
+
 # Build du projet
 npm run build
 
@@ -14,9 +16,17 @@ cp client/index.css draft/tmp/index.css
 cp -r client/public/assets draft/tmp/assets
 cp client/wasm/api.js draft/tmp/api.js
 cp client/wasm/api.wasm draft/tmp/api.wasm
+cp client/dist/bundle.js draft/tmp/bundle.js
+cp client/dist/worker.js draft/tmp/worker.js
 
 # Changement de branche git
 git switch gh-pages
 
 # Copies
 
+cp draft/tmp/index.css index.css
+cp -r draft/tmp/assets public/assets
+cp -r draft/tmp/api.js public/wasm/api.js
+cp -r draft/tmp/api.wasm public/wasm/api.wasm
+cp -r draft/tmp/bundle.js public/bundle.js
+cp -r draft/tmp/worker.js public/worker.js
